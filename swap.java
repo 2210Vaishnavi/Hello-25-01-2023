e7f7d26bec00d94c8f6753fa8e48ff3e239f144b
2210Vaishnavi <vaishnavikulkarni2210@gmail.com>
2023-01-25 15:26:03 +0530

    commiting 3 txt files and 1 swap java file

---
+++ swap.java
@@ -0,0 +1,11 @@
+public class swap{
+public static void main(String args[]){
+ int a = 10, b = 20;
+ int temp;
+ a = temp;
+ b = a;
+ temp = b;
+ System.out.println("Swapping of a=10 & b= 20 is: a= " + a+ " & b = " +b);
+ 
+}
+}
